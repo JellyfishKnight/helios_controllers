85790b9a9a65026118dd5677ba44399d1aa1e02b
JellyfishKnight <1002984726@qq.com>
2023-08-22 22:59:12 +0800

    Completed on_init() and command_interface_configuration

--- gm6020_controller_pkg/src/GM6020Controller.cpp
+++ gm6020_controller_pkg/src/GM6020Controller.cpp
@@ -1,18 +1,28 @@
 #include "GM6020Controller.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
 
 namespace helios_control {
 
-    controller_interface::return_type GM6020Controller::init(const std::string &controller_name) {
-        
-        return controller_interface::return_type::OK;
+    controller_interface::CallbackReturn GM6020Controller::on_init() {
+        try {
+            param_listener_ = std::make_shared<gm6020_controller::ParamListener>(get_node());
+            params_ = param_listener_->get_params();
+        } catch (const std::exception &e) {
+            RCLCPP_ERROR(logger_, "on_init: %s", e.what());
+            return controller_interface::CallbackReturn::ERROR;
+        }
+        return controller_interface::CallbackReturn::SUCCESS;
     }
 
-    controller_interface::InterfaceConfiguration GM6020Controller::command_interface_configuration() {
-        
-        
+    controller_interface::InterfaceConfiguration GM6020Controller::command_interface_configuration() const {
+        std::vector<std::string> conf_names;
+        for (const auto& joint_name : params_.motor_names) {
+            conf_names.push_back(joint_name + "/" + hardware_interface::HW_IF_VELOCITY);
+        }
+        return {controller_interface::interface_configuration_type::INDIVIDUAL, conf_names};
     }
 
-    controller_interface::InterfaceConfiguration GM6020Controller::state_interface_configuration() {
+    controller_interface::InterfaceConfiguration GM6020Controller::state_interface_configuration() const {
 
     }
 
