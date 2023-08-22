ed2b0d5dc510e33fcfed904ae810329a3bed4bea
JellyfishKnight <1002984726@qq.com>
2023-08-22 23:19:51 +0800

    Complete state_interface_configuration() and modified on_configure() and added some new params

--- gm6020_controller_pkg/src/GM6020Controller.cpp
+++ gm6020_controller_pkg/src/GM6020Controller.cpp
@@ -1,5 +1,4 @@
 #include "GM6020Controller.hpp"
-#include "hardware_interface/types/hardware_interface_type_values.hpp"
 
 namespace helios_control {
 
@@ -17,17 +16,32 @@ namespace helios_control {
     controller_interface::InterfaceConfiguration GM6020Controller::command_interface_configuration() const {
         std::vector<std::string> conf_names;
         for (const auto& joint_name : params_.motor_names) {
-            conf_names.push_back(joint_name + "/" + hardware_interface::HW_IF_VELOCITY);
+            conf_names.push_back(joint_name + "/" + "current");
         }
         return {controller_interface::interface_configuration_type::INDIVIDUAL, conf_names};
     }
 
     controller_interface::InterfaceConfiguration GM6020Controller::state_interface_configuration() const {
-
+        std::vector<std::string> conf_names;
+        for (const auto & joint_name : params_.motor_names) {
+            for (const auto & state_name : params_.motor_state_interfaces) {
+                conf_names.push_back(joint_name + "/" + state_name);
+            }
+        }
+        return {controller_interface::interface_configuration_type::INDIVIDUAL, conf_names};
     }
 
     controller_interface::CallbackReturn GM6020Controller::on_configure(const rclcpp_lifecycle::State &previous_state) {
-
+        if (param_listener_->is_old(params_)) {
+            params_ = param_listener_->get_params();
+            RCLCPP_INFO(logger_, "Parameters were updated");
+        }
+        if (params_.motor_names.size() != 4) {
+            RCLCPP_ERROR(logger_, "The number of motors is not 4");
+            return controller_interface::CallbackReturn::ERROR;
+        }
+        cmd_timeout_ = std::chrono::milliseconds{static_cast<int>(params_.cmd_timeout)};
+        
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
