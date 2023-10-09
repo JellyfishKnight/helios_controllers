307da77e9fc6ff739ebf7299c9c965abd04c1ab7
JellyfishKnight <1002984726@qq.com>
2023-10-09 11:07:00 +0800

    only tf2 transform left to write, others have been finished

--- imu_controller/include/imu_controller/IMUController.hpp
+++ imu_controller/include/imu_controller/IMUController.hpp
@@ -17,8 +17,14 @@
 #include "realtime_tools/realtime_box.h"
 #include "realtime_tools/realtime_publisher.h"
 #include "rclcpp_lifecycle/state.hpp"
-#include "lifecycle_msgs/msg/state.hpp"
 
+#include "tf2/convert.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster.h"
+
+
+#include "lifecycle_msgs/msg/state.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 
@@ -35,6 +41,12 @@
 
 
 namespace helios_control {
+
+constexpr auto DEFAULT_COMMAND_OUT_TOPIC = "/imu_out";
+
+using Params = imu_controller::Params;
+using ParamsListener = imu_controller::ParamListener;
+
 
 class IMUController : public controller_interface::ControllerInterface {
 public:
@@ -65,12 +77,39 @@ public:
     IMU_CONTROLLER_PUBLIC
     controller_interface::return_type update(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 private:
+    std::shared_ptr<realtime_tools::RealtimePublisher<sensor_msgs::msg::Imu>> realtime_imu_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
 
+    // Parameters from ROS for IMU
+    std::shared_ptr<ParamsListener> param_listener_;
+    Params params_;
+
+    // publish rate limiter
+    double publish_rate_ = 1000.0;
+    rclcpp::Duration publish_period_ = rclcpp::Duration::from_nanoseconds(0);
+    rclcpp::Time previous_publish_timestamp_{0, 0, RCL_CLOCK_UNINITIALIZED};
+
+    // tf2 utitlities
+    geometry_msgs::msg::TransformStamped transform_;
+    std::unique_ptr<tf2_ros::StaticTransformBroadcaster> static_pub_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> dynamic_pub_;
+
+
+    /**
+     * @brief Convert the current state of the chassis from state_interfaces to a ROS message
+     * @param state_msg The message to be filled
+     */
+    bool export_state_interfaces(sensor_msgs::msg::Imu& state_msg);
+
+
+    bool should_publish_ = false;
     bool is_halted_ = false;
     bool subscriber_is_active_ = false;
     bool reset();
     void halt();
 
+
+    rclcpp::Logger logger_ = rclcpp::get_logger("imu_controller");
 };
 
 
--- imu_controller/src/IMUController.cpp
+++ imu_controller/src/IMUController.cpp
@@ -13,44 +13,167 @@
 #include "IMUController.hpp"
 
 namespace helios_control {
+
+
 controller_interface::CallbackReturn IMUController::on_init() {
-    
+  // create params
+  try {
+    param_listener_ = std::make_shared<ParamsListener>(get_node());
+    params_ = param_listener_->get_params();
+  } catch (const std::exception &e) {
+    RCLCPP_ERROR(logger_, "on_init: %s", e.what());
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  static_pub_ = std::make_unique<tf2_ros::StaticTransformBroadcaster>(*this->get_node());
+  dynamic_pub_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this->get_node());
+  return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::InterfaceConfiguration IMUController::command_interface_configuration() const {
-    
+  std::vector<std::string> conf_names;
+  return {controller_interface::interface_configuration_type::INDIVIDUAL, conf_names};
 }
 
 controller_interface::InterfaceConfiguration IMUController::state_interface_configuration() const {
-    
+  std::vector<std::string> conf_names;
+  for (const auto & sensor_name : params_.sensor_names) {
+    for (const auto & state_name : params_.state_interfaces) {
+      conf_names.push_back(sensor_name + "/" + state_name);
+    }
+  }
+  return {controller_interface::interface_configuration_type::INDIVIDUAL, conf_names};
 }
 
 controller_interface::CallbackReturn IMUController::on_configure(const rclcpp_lifecycle::State &previous_state) {
-
+  // update parameters if they have changed
+  if (param_listener_->is_old(params_)) {
+    params_ = param_listener_->get_params();
+    RCLCPP_INFO(logger_, "Parameters were updated");
+  }
+  if (!reset()) {
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  // create publisher
+  imu_pub_ = get_node()->create_publisher<sensor_msgs::msg::Imu>(
+    DEFAULT_COMMAND_OUT_TOPIC, rclcpp::SystemDefaultsQoS()
+  );
+  realtime_imu_pub_ = std::make_shared<realtime_tools::RealtimePublisher<sensor_msgs::msg::Imu>>(
+    imu_pub_
+  );
+  // set publish rate
+  publish_rate_ = params_.publish_rate;
+  publish_period_ = rclcpp::Duration::from_seconds(1.0 / publish_rate_);
+  return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn IMUController::on_activate(const rclcpp_lifecycle::State & previous_state) {
-    
+  is_halted_ = false;
+  subscriber_is_active_ = true;
+  RCLCPP_DEBUG(get_node()->get_logger(), "Subscriber and publisher are now active.");
+  return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn IMUController::on_deactivate(const rclcpp_lifecycle::State & previous_state) {
-    
+  subscriber_is_active_ = false;
+  if (!is_halted_) {
+    halt();
+    is_halted_ = true;
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn IMUController::on_cleanup(const rclcpp_lifecycle::State& previous_state) {
-    
+  if (!reset()) {
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::return_type IMUController::update(const rclcpp::Time &time, const rclcpp::Duration &period) {
-    
+  if (get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE) {
+    if (!is_halted_) {
+      halt();
+      is_halted_ = true;
+    }
+    return controller_interface::return_type::OK;
+  }
+  // update params if they have changed
+  if (param_listener_->is_old(params_)) {
+    params_ = param_listener_->get_params();
+    RCLCPP_DEBUG(logger_, "Parameters were updated");
+  }
+  try {
+    if (previous_publish_timestamp_ + publish_period_ < time) {
+      previous_publish_timestamp_ += publish_period_;
+      should_publish_ = true;
+    }
+  } catch (std::runtime_error &e) {
+    // Handle exceptions when the time source changes and initialize publish timestamp
+    previous_publish_timestamp_ = time;
+    should_publish_ = true;
+  }
+  // publish gimbal states
+  if (should_publish_) {
+    if (realtime_imu_pub_->trylock()) {
+      auto & state_msg = realtime_imu_pub_->msg_;
+      geometry_msgs::msg::TransformStamped transform_stamped;
+      state_msg.header.stamp = time;
+      if (!export_state_interfaces(state_msg)) {
+        RCLCPP_WARN(logger_, "Could not find some state interfaces");
+      } else {
+        realtime_imu_pub_->unlockAndPublish();
+        transform_stamped.header.frame_id = params_.imu_frame_id;
+        transform_stamped.child_frame_id = params_.imu_child_frame_id;
+        transform_stamped.header.stamp = realtime_imu_pub_->msg_.header.stamp;
+      }
+    }
+  }
+  return controller_interface::return_type::OK;
 }
 
 bool IMUController::reset() {
-
+  static_pub_.reset();
+  dynamic_pub_.reset();
+  imu_pub_.reset();
+  realtime_imu_pub_.reset(); 
+  return true;
 }
 void IMUController::halt() {
     
 }
+
+bool IMUController::export_state_interfaces(sensor_msgs::msg::Imu& state_msg) {
+  state_msg.header.stamp = this->get_node()->now();
+  state_msg.header.frame_id = params_.imu_frame_id;
+  // get state interfaces
+  for (const auto& state : state_interfaces_) {
+    if (state.get_prefix_name() == "imu") {
+      if (state.get_interface_name() == "x") {
+        state_msg.orientation.x = state.get_value();
+      } else if (state.get_interface_name() == "y") {
+        state_msg.orientation.y = state.get_value();
+      } else if (state.get_interface_name() == "z") {
+        state_msg.orientation.z = state.get_value();
+      } else if (state.get_interface_name() == "w") {
+        state_msg.orientation.w = state.get_value();
+      } else if (state.get_interface_name() == "angular_velocity_x") {
+        state_msg.angular_velocity.x = state.get_value();
+      } else if (state.get_interface_name() == "angular_velocity_y") {
+        state_msg.angular_velocity.y = state.get_value();
+      } else if (state.get_interface_name() == "angular_velocity_z") {
+        state_msg.angular_velocity.z = state.get_value();
+      } else if (state.get_interface_name() == "linear_acceleration_x") {
+        state_msg.linear_acceleration.x = state.get_value();
+      } else if (state.get_interface_name() == "linear_acceleration_y") {
+        state_msg.linear_acceleration.y = state.get_value();
+      } else if (state.get_interface_name() == "linear_acceleration_z") {
+        state_msg.linear_acceleration.z = state.get_value();
+      } 
+    }
+  }
+  return true;
+}
+
 
 } // namespace helios_control
 
